1666031f9e20077d51bdd8f0743c3a5bc9b20a6c
Shamshodbek Khazratov <marco1131mc@gmail.com>
2023-10-20 23:19:20 +0500

    auto deleting application

--- apps/product/models.py
+++ apps/product/models.py
@@ -50,12 +50,6 @@ class Review(models.Model):
     comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
-
-
-
-
-
-
 class Cart(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
--- apps/seller/signals.py
+++ apps/seller/signals.py
@@ -4,7 +4,6 @@ from django.db.models import F, Sum
 from apps.product.models import CartItem, CharacteristicQuantity
 from .models import Seller, SellerApplication
 
-
 @receiver(post_save, sender=SellerApplication)
 def create_seller_profile(sender, instance, created, **kwargs):
     if instance.status == 'approved':
@@ -13,15 +12,26 @@ def create_seller_profile(sender, instance, created, **kwargs):
         seller.address = instance.address
         seller.phone_number = instance.phone_number
         seller.save()
+        instance.delete()
+
     elif instance.status == 'pending' and created:
         seller, _ = Seller.objects.get_or_create(user=instance.user)
         seller.save()
 
+    elif instance.status == 'rejected':
+        instance.delete()
+
 @receiver(post_save, sender=SellerApplication)
 def save_seller_profile(sender, instance, **kwargs):
+    
     if instance.status == 'approved':
         seller, created = Seller.objects.get_or_create(user=instance.user)
         seller.save()
+        instance.delete()
+
+    elif instance.status == 'rejected':
+        instance.delete()
+
 
 @receiver(post_save, sender=CharacteristicQuantity)
 def update_cart_item(sender, instance, created, **kwargs):
