ebf96821696996cd2f86df2c927c9cc867c03c29
Shamshodbek Khazratov <marco1131mc@gmail.com>
2023-12-08 19:03:10 +0500

    product filter fix

--- apps/api/router.py
+++ apps/api/router.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('categories/', CategoryListCreateView.as_view(), name='category-list-create'),
     path('category/<int:pk>/', CategoryRetrieveUpdateDestroyView.as_view(), name='category-rud'),
 
-    path('products/', YourProductView.as_view(), name='product-list-create'),
+    path('products/', ProductListCreateView.as_view(), name='product-list-create'),
     path('characteristics/', ProductCharacteristicList.as_view(), name='product-characteristics-list'),
     path('product/<int:pk>/', ProductRetrieveUpdateDestroyView.as_view(), name='product-rud'),
 
