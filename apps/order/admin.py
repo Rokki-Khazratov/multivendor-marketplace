f0d76a3beb59bc9cb9d2e66cfab366ee6d979404
Shamshodbek Khazratov <marco1131mc@gmail.com>
2024-01-30 19:52:27 +0500

    add order models

--- apps/order/admin.py
+++ apps/order/admin.py
@@ -1,3 +1,47 @@
+# admin.py
 from django.contrib import admin
+from .models import Order, OrderItem, OrderHistory, OneClickOrder, OneClickOrderItem
 
-# Register your models here.
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+class OneClickOrderItemInline(admin.TabularInline):
+    model = OneClickOrderItem
+    extra = 1
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user_profile', 'created_at', 'status', 'is_paid', 'display_total_price')
+    list_filter = ('status', 'is_paid')
+    search_fields = ('user_profile__user__username', 'user_profile__user__email')
+    inlines = [OrderItemInline]
+
+@admin.register(OrderHistory)
+class OrderHistoryAdmin(admin.ModelAdmin):
+    list_display = ('user_profile', 'orders_count')
+    search_fields = ('user_profile__user__username', 'user_profile__user__email')
+
+    def orders_count(self, obj):
+        return obj.orders.count()
+    orders_count.short_description = 'Number of Orders'
+
+@admin.register(OneClickOrder)
+class OneClickOrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'full_name', 'created_at', 'status')
+    list_filter = ('status',)
+    search_fields = ('full_name', 'email', 'phone_number')
+    inlines = [OneClickOrderItemInline]
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'characteristic', 'quantity', 'display_item_price')
+    search_fields = ('order__id', 'characteristic__name')
+
+    @admin.display(description='Item Price')
+    def display_item_price(self, obj):
+        return obj.display_item_price
+
+    def get_inline_instances(self, request, obj=None):
+        return []
+
--- apps/order/models.py
+++ apps/order/models.py
@@ -1,45 +1,99 @@
-# app name - order
-
+# models.py
 from django.db import models
-from django.contrib.auth.models import User
-from apps.product.models import  Product, ProductCharacteristic
+from apps.user.models import UserProfile
+from apps.product.models import ProductCharacteristic
+
+#signals:
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
+#* status of order
+PENDING = 1
+PROCESSING = 2
+DELIVERED = 3
+GOTTED = 4
+
+STATUS_CHOICES = [
+    (PENDING, 'В ожидании'), #orange
+    (PROCESSING, 'В пути'), #yellow
+    (DELIVERED, 'Доставлен и ожидает'), #GREEN !!!!
+    (GOTTED, 'Забран'), #gray
+]
+
+
+#!-----------------ORDER VIA user_profile----------------
 class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
-    shipping_address = models.TextField()
-    order_date = models.DateTimeField(auto_now_add=True)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    # updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return f"Order by {self.user.username}"
+    status = models.IntegerField(choices=STATUS_CHOICES, default=PENDING)
+    is_paid = models.BooleanField(default=False)
+    # payment_method = models.CharField(max_length=255, null=True, blank=True)
+    # shipping_address = models.TextField(null=True, blank=True)
 
-    @staticmethod
-    def create_order_from_cart(cart, shipping_address):
-        order = Order.objects.create(user=cart.user, cart=cart, shipping_address=shipping_address)
-        
-        for cart_item in cart.cartitem_set.all():
-            order_item = OrderItem(
-                order=order,
-                product=cart_item.product,
-                quantity=cart_item.quantity
-            )
-            order_item.save()
-        
-        return order
-
-class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField()
-    characteristics = models.ManyToManyField(ProductCharacteristic)
-
-    def get_total_price(self):
-        return self.product.price * self.quantity
 
     @property
-    def total_characteristics_quantity(self):
-        return sum(characteristic.quantity for characteristic in self.characteristics.all())
+    def display_total_price(self):
+        return sum(item.display_item_price * item.quantity for item in self.order_items.all())
+
 
     def __str__(self):
-        return f"Order Item for Order {self.order.id}"
+        return f"Order {self.id} - {self.user_profile.user.username if self.user_profile else 'Guest'}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_items')
+    characteristic = models.ForeignKey(ProductCharacteristic, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    @property
+    def display_item_price(self):
+        return self.characteristic.price
+
+    def __str__(self):
+        return f"{self.order} - {self.characteristic.name} : {self.quantity}"
+
+class OrderHistory(models.Model):
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    orders = models.ManyToManyField(Order, related_name='order_history')
+
+    def __str__(self):
+        return f"OrderHistory for {self.user_profile.user.username} - {self.orders.count()} Orders"
+    
+#!-----------------ORDER IN ONE CLICK----------------
+class OneClickOrder(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS_CHOICES, default=PENDING)
+
+
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
+    address = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f"One-Click Order {self.id} - {self.full_name}"
+
+class OneClickOrderItem(models.Model):
+    order = models.ForeignKey(OneClickOrder, on_delete=models.CASCADE, related_name='items')
+    characteristic = models.ForeignKey(ProductCharacteristic, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    @property
+    def display_item_price(self):
+        return self.characteristic.price
+
+    def __str__(self):
+        return f"One-Click Order {self.order.id} Item - {self.characteristic.name} : {self.quantity}"
+
+#! ----signals----
+@receiver(post_save, sender=Order)
+def move_order_to_history(sender, instance, **kwargs):
+    if instance.status == Order.GOTTED:
+        OrderHistory.objects.create(user_profile=instance.user_profile, order=instance)
